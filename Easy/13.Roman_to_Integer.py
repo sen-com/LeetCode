54796fe301f29dfee42242ea456e26c6e3114ffc
sen-com <nogenon@docomo.ne.jp>
2020-12-03 20:54:22 +0900

    13 done

---
+++ Easy/13.Roman_to_Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500 , "M": 1000}
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        sum = 0
+        for num in s:
+            sum += roman[num]
+        return sum
+
+        
