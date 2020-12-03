76ed9b7d2f1cee44c699faa31bdabeec4e65ca57
sen-com <nogenon@docomo.ne.jp>
2020-12-03 21:15:15 +0900

    done 13

--- .ipynb_checkpoints/7.Reverse_Integer-checkpoint.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x < 0:
-            x = int(str(x)[0] + str(x)[:0:-1])
-        else:
-            x = int(str(x)[::-1])
-        return (x if (-2**31 < x < 2**31 - 1) else 0)
